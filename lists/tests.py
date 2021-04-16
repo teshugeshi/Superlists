6a33610004c1b0bd9c97e77bbe19577762bd0ad9
monizz <caomingwei@bupt.edu.cn>
2021-04-16 14:12:20 +0800

    Too much test

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 
@@ -10,12 +11,13 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func,home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest() 
-        response=home_page(request)
-        html = response.content.decode('utf8')
+        response=self.client.get('/')
+        html=response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response,'home.html')
 
 """ class SmokeTest(TestCase):
 
