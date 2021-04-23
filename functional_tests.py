18d1f43916336394e43747443fa69c16ef389e79
monizz <caomingwei@bupt.edu.cn>
2021-04-23 14:22:30 +0800

    '2021.4.23.first'

--- functional_tests.py
+++ functional_tests.py
@@ -16,18 +16,16 @@ class NewVisitorTest(unittest.TestCase):
     self.assertIn('To-Do',header_text)
 
     inputbox =self.browser.find_element_by_id('id_new_item')
-    self.assertEqual(
-      inputbox.get_attribute('placeholder'),
-      'Enter a to-do item'
-      )
-
-    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
     time.sleep(1)
+
     table=self.browser.find_element_by_id('id_list_table')
-    row=table.find_elements_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text=='1:Buy peacock feathers' for row in rows)
+    rows=table.find_elements_by_tag_name('tr')
+    self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+    self.assertIn(
+      '2:Use peacock feathers to make a fly',
+      [row.text for row in rows]
     )
     self.fail('Finish the test!')
 
