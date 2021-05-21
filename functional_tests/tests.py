2a6bea9290ea50d6cbd51b5c1b6538f5455234e0
monizz <caomingwei@bupt.edu.cn>
2021-05-21 13:53:16 +0800

    7th 42

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.test.testcases import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -22,6 +23,10 @@ class NewVisitorTest(LiveServerTestCase):
         if time.time()-start_time>MAX_WAIT:
           raise e
         time.sleep(0.5)
+  def test_redirects_after_POST(self):
+    response=self.client.post('/',data={'item_text':'A new list item'})
+    self.assertEqual(response.status_code,302)
+    self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
   def test_can_start_a_list_for_one_user(self):
     self.browser.get(self.live_server_url)
     self.assertIn('To-Do',self.browser.title)
@@ -41,7 +46,6 @@ class NewVisitorTest(LiveServerTestCase):
 
     self.wait_for_row_in_list_table('1: Buy peacock feathers')
     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-    self.fail('Finish the test!')
   def test_multiple_users_can_start_lists_at_different_urls(self):
     self.browser.get(self.live_server_url)
     inputbox=self.browser.find_element_by_id('id_new_item')
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,21 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+from django.http import HttpRequest, response
 from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
+class ListViewTest(TestCase):
+  def test_uses_list_template(self):
+      response=self.client.get('/lists/the-only-list-in-the-world/')
+      self.assertTemplateUsed(response,'list.html')
+  def test_displays_all_items(self):
+    Item.objects.create(text='itemey 1')
+    Item.objects.create(text='itemey 2')
+    
+    response=self.client.get('/lists/the-only-list-in-the-world/')
 
+    self.assertContains(response,'itemey 1')
+    self.assertContains(response,'itemey 2')
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item=Item()
@@ -22,16 +33,7 @@ class ItemModelTest(TestCase):
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Item the second')
-
 class HomePageTest(TestCase):
-    def test_displays_all_list_items(self):
-        Item.objects.create (text='itemey 1')
-        Item.objects.create (text='itemey 2')
-
-        response=self.client.get('/')
-
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
@@ -44,7 +46,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response=self.client.post('/',data={'item_text':'A new list item'})
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
