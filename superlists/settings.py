a941a974778fc0be9d8d7075a706f796e8209299
monizz <caomingwei@bupt.edu.cn>
2021-06-09 10:58:32 +0800

    9th second

--- superlists/settings.py
+++ superlists/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -25,7 +24,7 @@ SECRET_KEY = '=%k4rvq*hd$k7dhr=m6rg3a=(8&jxf^q+fu(tt-6hl6e(_kv-6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'database/db.sqlite3'),
     }
 }
 
